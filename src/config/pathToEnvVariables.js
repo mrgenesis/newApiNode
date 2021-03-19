35c5466d66bbbd56a9edc58645d3b43a80111052
Rosa, Genesis <mrgenesis@hotmail.com>
2021-03-19 14:28:46 -0400

    deletado arquivos desnecessários

--- src/config/pathToEnvVariables.js
+++
@@ -1,6 +0,0 @@
-const { resolve } = require('path');
-module.exports = {
-
-  // Este é o caminho onde consta o arquivo com as variáveis de ambiente para desovolvimento em localhost
-  pathToEnvVariables: resolve('localhost', '.env')
-};
