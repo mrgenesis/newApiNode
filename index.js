9a42c5eaa5b22ba3d70228b32ffce288b0182147
Rosa, Genesis <mrgenesis@hotmail.com>
2021-04-01 18:15:58 -0400

    o autoloader passou a retornar uma promise, assim foi preciso fazer readequação

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-//Este quivo é o ponto de partida do aplicativo. Ele deve ser executado tanto em ambiente de desenvolvimento quanto em ambiente de produção.
-//Antes da exucução do programa, é necessário definir as variáveis de ambiente para o correto funcionamento do aplicativo.
-const app = require('./src/start')
-  , port = process.env.PORT || 3000;
+//Este quivo é o ponto de partida do aplicativo. Ele deve ser executado tanto em
+// ambiente de desenvolvimento quanto em ambiente de produção. Antes da exucução
+// do programa, é necessário definir as variáveis de ambiente para o correto
+// funcionamento do aplicativo.
+const starting = require('./src/start');
 
-app.listen(port, () => console.log('>>> Running at port ' + port + '...'));
+starting.then(started => {
+  started.app.listen(started.port, () =>
+    console.log('>>> Running at port ' + started.port + '...'));
+});
--- src/start.js
+++ src/start.js
@@ -1,3 +1,13 @@
-const app = require('./autoloader');
+/**
+ * O objetivo deste arquivo é iniciar o aplicativo. Ele é chamado pelo arquivo
+ * index.js na root do projeto. E deve ser chamado diretamente quando estiver
+ * executando testes. @see https://www.npmjs.com/package/supertest
+ * @author Rosa, Genesis @see https://linkedin.com/in/mrgenesis
+ */
+const start = require('./autoloader')
+  , starting = start();
 
-module.exports = app;
+
+// Retorna uma promise que resolve um objeto com o app e port "{ app, port }"
+// starting.then({app, port} => app.listen(port))
+module.exports = starting;
