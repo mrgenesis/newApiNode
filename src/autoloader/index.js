ff49e7cf835cbebf23a1981c1906c4276c11d737
Rosa, Genesis <mrgenesis@hotmail.com>
2021-04-01 18:13:46 -0400

    readequado o arquivo de rotas e texto do comentário

--- src/autoloader/index.js
+++ src/autoloader/index.js
@@ -1,41 +1,33 @@
 /**
- * O objetivo deste arquivo é iniciar os recursos na sequencia correta.
- * O resultado será a exposição do app configurado com todos os recursos necessários diponíveis
+ * O objetivo deste arquivo é iniciar os recursos na sequencia correta. O
+ * resultado será a exposição do app configurado com todos os recursos 
+ * necessários diponíveis
+ * @author Rosa, Genesis @see https://linkedin.com/in/mrgenesis
  */
 const
-  path = require('path')
-  , fs = require('fs')
-  , https = require('https')
-  , variablesEnvironment = require(path.resolve(__dirname, 'variablesEnvironment'))
+  { join } = require('path')
+  , variablesEnvironment = require(join(__dirname, 'variablesEnvironment'))
 
 module.exports = async () => {
 
-  // Este é um recurso que prove uma maneira de disponibilizar variáveis de ambiente localmente.
-  // Se o valor definido em `process.env.NODE_ENV` for DIFERENTE de 
-  // `production`, o aplicativo entenderá que as variáveis de ambiente precisam
-  // ser definidas em modo `development`. E se for preciso, baixará um arquivo
-  // `.env` à pasta padrão em path/to/PROJECT/localhost/.env
+  // Este é um recurso que prove uma maneira de disponibilizar variáveis de 
+  // ambiente localmente. Se o valor definido em `process.env.NODE_ENV` for 
+  // DIFERENTE de `production`, as variáveis de ambiente serão definidas em modo
+  // `development`. E se for preciso, baixará um arquivo `.env` da internet à 
+  // pasta padrão em path/to/PROJECT/localhost/.env.
   await variablesEnvironment(process.env.NODE_ENV);
-  process.exit(0);
 
 
-  if (process.env.NODE_ENV === 'production') {
-    console.log(` --> [autoloader]: O ambiente está definido como "production". As variáveis devem ser definidas na camada da hospedagem.`);
-  } else {
-    if (!await setLocalVariables(devEnvVarPath, getRemoteFile)) {
-      console.log('Falha na definição das variáveis de ambiente.');
-    }
-  }
+  // Configura e obtem a biblioteca que lida com as requisições http.
+  const { app, router } = require(join(__dirname, 'HTTPServer'));
 
-  // Obtem a bibliotéca que lida com as requisições http com os recursos adicionais carreagados.
-  const app = require(path.join(__dirname, 'HTTPServer'));
 
-  //let t = fs.readdirSync(__dirname);
-  //t = t.filter(f => f[0] !== '.' && path.extname(f) === '.js' && f !== 'index.js')
-  const router = require('./router');
+  // Carrega os arquivos de configuração das rotas
+  require(join(__dirname, '..', 'route'))(router);
 
+
+  // Adiciona as rotas ao aplicativo
   app.use(router);
 
-
-  return app;
+  return { app, port: process.env.PORT || 3000 };
 };
